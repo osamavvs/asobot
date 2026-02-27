799a7adec2758321392aac227a1810b2a5f2262b
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 18:35:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-import os
-
-# بيانات التليجرام
+# config.py
 API_ID = 24752047
 API_HASH = "5b8a468627791bdd36a8c361913b0b72"
-# التوكن الجديد الذي قدمته
 BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
 
-# الإعدادات الإضافية
+# الاشتراك الاجباري
 FORCE_SUB = "BBABB9"
+
+# كروب التخزين
 LOG_GROUP = -1005129765852
