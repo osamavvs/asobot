baea31721d4c14e7b348483379baf358719d7e41
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 05:57:27 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,15 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # --- حقوق سورس كرستال ---
 MUSIC_BOT_NAME = "بوت كرستال للموسيقى" 
-OWNER_ID = 12345678 # ضع الأيدي الخاص بك هنا
-SUPPORT_CHANNEL = "https://t.me/YourChannel" # قناتك
-SUPPORT_GROUP = "https://t.me/YourGroup" # جروبك
+OWNER_ID = 8074717568 # ضع الـ ID الخاص بك هنا (أرقام فقط)
+SUPPORT_CHANNEL = "https://t.me/BBABB9" # رابط قناتك
+رابط مجموعتك
 
-# --- إعدادات التشغيل ---
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MONGO_DB_URI = os.getenv("MONGO_DB_URI")
-STRING_SESSION = os.getenv("STRING_SESSION")
+# --- إعدادات التشغيل المباشرة ---
+# استبدل الأرقام والرموز بالبيانات التي استخرجتها
+API_ID = 24752047           # ضع رقم الـ API_ID هنا (بدون علامات تنصيص)
+API_HASH = "5b8a468627791bdd36a8c361913b0b72"  # ضع الـ API_HASH هنا (بين علامات تنصيص)
+BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c" # توكن البوت من BotFather (بين علامات تنصيص)
+MONGO_DB_URI = "mongodb+srv://asopes111_db_user:urKzCqP4yrMpSUyq@cluster0.haza5xq.mongodb.net/?appName=Cluster0" # رابط قاعدة البيانات (بين علامات تنصيص)
+STRING_SESSION = "AgF3lL4ApOdDQh8mSgGF7ONSKWF5JrJ3gxBj59HFew8FTo_qpyh6W0YF9Ay4OQrwuRchJ87c3SjUFzU-VPhqB-mfkthG16WgNQX-Pze6VpcXQ7tN25L0eDa65OPcrVpo_SHd0rKi5ApNPiRPdypZkByPm2wrxoVR7GQQL2juO-YXh6OgoWAsF0K1rZFVUk1MzTYLyKT7io1qNQUncniNWJtLWMhkVCr5igCCVtPnmyIHUU5oRF1OTV-Qdhq6Ij621zlqL9NtjzJxVahdZAOagK-T-yOnSvlzcNtjew3cmNMyPcy8vGQNwNOpqLDBDr5R8v6abL0o949hXy_u7FiHDoefqwCW5gAAAAHb4UXaAA"    # كود الجلسة (بين علامات تنصيص)
