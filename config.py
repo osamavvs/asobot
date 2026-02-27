dd28080e4dd6174cad71652707a291c7f8a276ab
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 17:56:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 import os
 
-# --- حقوق سورس كرستال ---
-MUSIC_BOT_NAME = "بوت كرستال للموسيقى" 
-OWNER_ID = 8074717568 # الـ ID الخاص بك
-OWNER_USER = "U_K44" # يوزرك بدون @ (هذا السطر كان ناقصاً)
-CHANNEL_USER = "BBABB9" # يوزر القناة بدون @ (هذا السطر كان ناقصاً)
-SUPPORT_CHANNEL = "https://t.me/BBABB9" # رابط قناتك
+# بيانات التليجرام
+API_ID = 24752047
+API_HASH = "5b8a468627791bdd36a8c361913b0b72"
+# التوكن الجديد الذي قدمته
+BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
 
-# --- إعدادات التشغيل المباشرة ---
-API_ID = 24752047           
-API_HASH = "5b8a468627791bdd36a8c361913b0b72"  
-BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c" 
-MONGO_DB_URI = "mongodb+srv://asopes111_db_user:urKzCqP4yrMpSUyq@cluster0.haza5xq.mongodb.net/?appName=Cluster0" 
-STRING_SESSION = "AgF3lL4ApOdDQh8mSgGF7ONSKWF5JrJ3gxBj59HFew8FTo_qpyh6W0YF9Ay4OQrwuRchJ87c3SjUFzU-VPhqB-mfkthG16WgNQX-Pze6VpcXQ7tN25L0eDa65OPcrVpo_SHd0rKi5ApNPiRPdypZkByPm2wrxoVR7GQQL2juO-YXh6OgoWAsF0K1rZFVUk1MzTYLyKT7io1qNQUncniNWJtLWMhkVCr5igCCVtPnmyIHUU5oRF1OTV-Qdhq6Ij621zlqL9NtjzJxVahdZAOagK-T-yOnSvlzcNtjew3cmNMyPcy8vGQNwNOpqLDBDr5R8v6abL0o949hXy_u7FiHDoefqwCW5gAAAAHb4UXaAA"
+# الإعدادات الإضافية
+FORCE_SUB = "BBABB9"
+LOG_GROUP = -1005129765852
