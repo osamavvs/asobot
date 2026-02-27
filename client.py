da86a5d4aaec476b609ca1e8111a3d6f1cfe5ecb
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 20:51:51 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,9 +1,17 @@
 from pyrogram import Client
-from pytgcalls import Client as PyTgCallsClient # الاسم الجديد في الإصدار v3
 from config import API_ID, API_HASH, BOT_TOKEN
+import pytgcalls
 
-# تهيئة التليجرام
+# فحص النسخة ديناميكياً وتحديد الكلاينت المناسب
+if hasattr(pytgcalls, "Client"):
+    # إذا كانت النسخة v3
+    PyTgCalls = pytgcalls.Client
+elif hasattr(pytgcalls, "PyTgCalls"):
+    # إذا كانت النسخة v2
+    PyTgCalls = pytgcalls.PyTgCalls
+else:
+    # حل احتياطي أخير
+    from pytgcalls import PyTgCalls
+
 app = Client("CristalBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-# تهيئة الاتصال الصوتي بالاسم الجديد
-call_py = PyTgCallsClient(app)
+call_py = PyTgCalls(app)
