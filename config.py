84824a3579bc980ac0dc90971190003cd74d00a7
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 19:32:57 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# بيانات التليجرام (لا تغير القيم)
-API_ID = 24752047
-API_HASH = "5b8a468627791bdd36a8c361913b0b72"
-BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
-FORCE_SUB = "BBABB9"
