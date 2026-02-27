d2508fa42b252026b8fad2e89e328a5a7a6395a8
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 07:59:11 +0300

    Update bot.py

--- CristalMusic/bot.py
+++ CristalMusic/bot.py
@@ -1,19 +1,9 @@
-import asyncio
-import pyrogram.errors
+async def main():
+    await app.start()
+    await call_py.start()
+    print("🚀 البوت والمحرك الصوتي جاهزان!")
+    from pyrogram import idle
+    await idle()
 
-# هذا الكود يمنع انهيار البوت إذا لم يجد تعريف الخطأ في المكتبة
-try:
-    from pyrogram.errors import GroupcallForbidden
-except ImportError:
-    # صنع الخطأ يدوياً في ذاكرة النظام لتجاوز مشكلة الاستيراد
-    setattr(pyrogram.errors, "GroupcallForbidden", type("GroupcallForbidden", (Exception,), {}))
-
-from pyrogram import Client, filters
-from pytgcalls import PyTgCalls
-from pytgcalls.types import MediaStream
-from yt_dlp import YoutubeDL
-from config import API_ID, API_HASH, BOT_TOKEN
-
-# تعريف البوت والمحرك
-app = Client("CristalBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-call_py = PyTgCalls(app)
+if __name__ == "__main__":
+    asyncio.run(main())
