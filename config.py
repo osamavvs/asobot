82457621a24fbde07e02a8746c46d26d315fbe23
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 19:53:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
 
+API_ID = 1234567  # أيديك من my.telegram.org
+API_HASH = "your_hash"  # الهاش من my.telegram.org
+BOT_TOKEN = "your_token"  # توكن البوت من @BotFather
+
+OWNER_ID = 8074717568
+OWNER_USER = "U_K44"
+CHANNEL_USER = "BBABB9"
