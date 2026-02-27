e0b3ee6d72999ef763dfe18abc3af8868e23cf80
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 18:53:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 import os
 
-# بيانات التليجرام الأساسية
+# بيانات التليجرام (لا تغير القيم)
 API_ID = 24752047
 API_HASH = "5b8a468627791bdd36a8c361913b0b72"
 BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
-
-# إعدادات إضافية
-FORCE_SUB = "BBABB9" # يوزر قناة الاشتراك الإجباري بدون @
+FORCE_SUB = "BBABB9"
