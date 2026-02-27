600ce7f9a0af6c9bd5f396d941fe27c9482022fe
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-02-27 20:12:21 +0300

    Update client.py

--- core/client.py
+++ core/client.py
@@ -5,5 +5,5 @@ from config import API_ID, API_HASH, BOT_TOKEN
 # تهيئة تطبيق التليجرام
 app = Client("CristalBot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# تهيئة مشغل المكالمات (باستخدام الاستدعاء الصحيح للنسخة الحديثة)
+# في النسخ الحديثة يتم الاستدعاء هكذا
 call_py = PyTgCalls(app)
